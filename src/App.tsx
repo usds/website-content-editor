4014415bd4ff6d5a2a27640ccf95cfcfea61c408
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2024-03-06 18:34:34 -0800

    Fix nav menu disappearing when not 1024px wide
    We aren't concerned about being mobile friendly right now. The editor works best in desktop. But the USWDS topnav has a css breakpoint that's 1024px wide. Replace <PrimaryNav> and css to make it always show up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import {ToastContainer} from "react-toastify";
 
-import '@mdxeditor/editor/style.css';
-import './styles/usdswebsite.override.css';
-import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
 import {Fragment, useReducer} from "react";
 import {BlogEditorPage} from "./pages/BlogEditorPage.tsx";
-import {Header, PrimaryNav, Grid, GridContainer} from "@trussworks/react-uswds";
+import {Header as USWDSHeader, Grid, GridContainer} from "@trussworks/react-uswds";
 import {HomePage} from "./pages/HomePage.tsx";
 import {AboutPage} from "./pages/AboutPage.tsx";
 
+import '@mdxeditor/editor/style.css';
 // we use the uswds styles from usds.github.io/website-staging
 // import '@uswds/uswds/css/uswds.css';
+import './styles/usdswebsite.override.css';
+import './styles/uswds.navmenu.override.css';
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
 
 // roll our own nav. Doing anything too fancy is difficult on github pages because of paths.
 // keys are UX names which isn't great but whatever. It's just a few lines of code.
@@ -28,7 +30,6 @@ function App() {
     localStorage.setItem('currentPage', cur);
     return cur;
   }, (localStorage.getItem('currentPage') as PagesType) || "Home");
-
   const PAGES_MAP: PagesMap = {
     "Home": <HomePage/>,
     "Blog Edit": <BlogEditorPage/>,
@@ -37,24 +38,31 @@ function App() {
 
   return (
     <Fragment>
-      <Header basic>
+
+      <USWDSHeader basic>
         <div className={"float-left position-absolute top-1 left-105 text-bold"}>
           Website Content Editor
         </div>
-        <div className="usa-nav-container">
-          <PrimaryNav
-            aria-label="Primary navigation"
-            items={Pages.map((eachPage) =>
-              <a key={`${eachPage}`}
-                 href=""
-                 className={eachPage === page ? "usa-nav__link usa-current" : "usa-nav__link"}
-                 onClick={(e) => {
-                   setPage(eachPage);
-                   e.preventDefault();
-                   e.stopPropagation();
-                 }}>{eachPage}</a>)}/>
+        {/* We could use <PrimaryNav> here but the css breakpoint for the mobile is annoying */}
+        <div className="usa-nav-custom-container">
+          <nav className="usa-nav-custom" aria-label="Primary navigation">
+            <ul className="usa-nav-custom__primary usa-accordion">
+              {Pages.map((eachPage) =>
+                <li key={eachPage} className="usa-nav-custom__primary-item">
+                  <a href="" type="primary"
+                     className={eachPage === page ? "usa-nav-custom__link usa-current" : "usa-nav-custom__link"}
+                     onClick={(e) => {
+                       setPage(eachPage);
+                       e.preventDefault();
+                       e.stopPropagation();
+                     }}>{eachPage}</a>
+                </li>)}
+            </ul>
+          </nav>
         </div>
-      </Header>
+      </USWDSHeader>
+
+
       <div className="usa-section">
         <GridContainer>
           <Grid row gap>
